6a8de4d4536900b8edf15f0476604433f704cb85
experizhal <experizhal@gmail.com>
2014-08-23 01:26:02 +0700

    render

--- protected/controllers/PermintaanController.php
+++ protected/controllers/PermintaanController.php
@@ -82,5 +82,41 @@ class PermintaanController extends Controller {
         $this->render('upload', array('form' => $form));
     }
     
+    public function actionF_permintaan_f(){
+    	$model = new Permintaan;
+
+        // uncomment the following code to enable ajax-based validation
+        /*
+          if(isset($_POST['ajax']) && $_POST['ajax']==='permintaan-f_permintaan-form')
+          {
+          echo CActiveForm::validate($model);
+          Yii::app()->end();
+          }
+         */
+
+        if (isset($_POST['Permintaan'])) {
+            $model->attributes = $_POST['Permintaan'];
+            if ($model->validate()) {
+                // form inputs PERMINTAAN BUKU BARU
+                $model->id_anggota = $_POST['Permintaan']['id_anggota'];
+                $model->judul = $_POST['Permintaan']['judul'];
+                $model->jenis = $_POST['Permintaan']['jenis'];
+                $model->pengarang = $_POST['Permintaan']['pengarang'];
+                $model->penerbit = $_POST['Permintaan']['penerbit'];
+                $model->tahun_terbit = $_POST['Permintaan']['tahun_terbit'];
+                $model->kota = $_POST['Permintaan']['kota'];
+                $model->edisi = $_POST['Permintaan']['edisi'];
+                $model->isbn = $_POST['Permintaan']['isbn'];
+                $model->keterangan = $_POST['Permintaan']['keterangan'];
+                $model->save();
+                $this->redirect(array('Permintaan/Index'));
+                return;
+            }
+        }
+        $data = Permintaan::model()->findAll();
+		//$this->render('listPermintaan',array('data'=>$data));
+        $this->render('f_permintaan_f', array('model' => $model,'data'=>$data));
+	}
+    
 
 }
--- protected/views/permintaan/f_permintaan_f.php
+++ protected/views/permintaan/f_permintaan_f.php
@@ -1,30 +1,14 @@
-<div class="tab-pane" id="Fakultas">
-  <div class="box box-primary">
-        <div class="box-header">
-            <h4 class="box-title">PERMINTAAN BUKU BARU</h4>
-        </div>
-  <?php echo $form->errorSummary($model); ?>
-	
-	<div class="form-group">
-		<?php echo $form->labelEx($model,'id_anggota',array('class' => 'col-sm-2 control-label')); ?>
-		<div class="col-sm-10">
-		<?php echo $form->textField($model,'id_anggota',array('class' => 'form-control','placeholder' => 'id_anggota')); ?>
-		<?php echo $form->error($model,'id_anggota'); ?>
-		</div>
-	</div>
-    
-    <div class="form-group">
-        <div class="col-sm-2 control-label"><b>Masukkan Data Excel</b></div>
-        <div class="col-sm-10">
-        <?php echo $form->fileField($model,'filee',array('size'=>60, 'maxlength'=>200,'class' => 'btn btn-default')); ?>
-       </div>
-           
-	<div class="form-group">
-	<div class="col-sm-2 control-label">
-		<?php echo CHtml::submitButton('Submit',array('class' => 'btn btn-default')); ?>
-	</div>
-    </div>
-    
-    </div>
-  </div>
-  </div>
+<!-- Nav tabs -->
+<ul class="nav nav-tabs" role="tablist">
+  <li class="active"><a href="#home" role="tab" data-toggle="tab">FORM</a></li>
+  <li><a href="#profile" role="tab" data-toggle="tab">UPLOAD</a></li>
+  <li><a href="#messages" role="tab" data-toggle="tab">DAFTAR PERMINTAAN</a></li>
+</ul>
+
+<!-- Tab panes -->
+<div class="tab-content">
+  <div class="tab-pane active" id="home">...</div>
+  <div class="tab-pane" id="profile">...</div>
+  <div class="tab-pane" id="messages">...</div>
+  <div class="tab-pane" id="settings">...</div>
+</div>
