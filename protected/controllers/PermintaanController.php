8b4c82675601c0079bb7f4ab994d3fe1076e3d1d
experizhal <experizhal@gmail.com>
2014-08-22 15:19:35 +0700

    controler permintaan

--- protected/controllers/PermintaanController.php
+++ protected/controllers/PermintaanController.php
@@ -1,60 +1,68 @@
 <?php
 
-class PermintaanController extends Controller
-{
-	public function actionIndex()
-	{
-		$this->render('index');
-	}
+class PermintaanController extends Controller {
 
-	// Uncomment the following methods and override them if needed
-	/*
-	public function filters()
-	{
-		// return the filter configuration for this controller, e.g.:
-		return array(
-			'inlineFilterName',
-			array(
-				'class'=>'path.to.FilterClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
+    public function actionIndex() {
+        $this->render('index');
+    }
 
-	public function actions()
-	{
-		// return external action classes, e.g.:
-		return array(
-			'action1'=>'path.to.ActionClass',
-			'action2'=>array(
-				'class'=>'path.to.AnotherActionClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
-	*/
-	public function actionF_permintaan()
-{
-    $model=new Permintaan;
-
-    // uncomment the following code to enable ajax-based validation
+    // Uncomment the following methods and override them if needed
     /*
-    if(isset($_POST['ajax']) && $_POST['ajax']==='permintaan-f_permintaan-form')
-    {
-        echo CActiveForm::validate($model);
-        Yii::app()->end();
-    }
-    */
+      public function filters()
+      {
+      // return the filter configuration for this controller, e.g.:
+      return array(
+      'inlineFilterName',
+      array(
+      'class'=>'path.to.FilterClass',
+      'propertyName'=>'propertyValue',
+      ),
+      );
+      }
 
-    if(isset($_POST['Permintaan']))
-    {
-        $model->attributes=$_POST['Permintaan'];
-        if($model->validate())
-        {
-            // form inputs are valid, do something here
-            return;
+      public function actions()
+      {
+      // return external action classes, e.g.:
+      return array(
+      'action1'=>'path.to.ActionClass',
+      'action2'=>array(
+      'class'=>'path.to.AnotherActionClass',
+      'propertyName'=>'propertyValue',
+      ),
+      );
+      }
+     */
+    public function actionF_permintaan() {
+        $model = new Permintaan;
+
+        // uncomment the following code to enable ajax-based validation
+        /*
+          if(isset($_POST['ajax']) && $_POST['ajax']==='permintaan-f_permintaan-form')
+          {
+          echo CActiveForm::validate($model);
+          Yii::app()->end();
+          }
+         */
+
+        if (isset($_POST['Permintaan'])) {
+            $model->attributes = $_POST['Permintaan'];
+            if ($model->validate()) {
+                // form inputs PERMINTAAN BUKU BARU
+                $model->id_anggota = $_POST['Permintaan']['id_anggota'];
+                $model->judul = $_POST['Permintaan']['judul'];
+                $model->jenis = $_POST['Permintaan']['jenis'];
+                $model->pengarang = $_POST['Permintaan']['pengarang'];
+                $model->penerbit = $_POST['Permintaan']['penerbit'];
+                $model->tahun_terbit = $_POST['Permintaan']['tahun_terbit'];
+                $model->kota = $_POST['Permintaan']['kota'];
+                $model->edisi = $_POST['Permintaan']['edisi'];
+                $model->isbn = $_POST['Permintaan']['isbn'];
+                $model->keterangan = $_POST['Permintaan']['keterangan'];
+                $model->save();
+                return;
+            }
         }
+        $this->render('f_permintaan', array('model' => $model));
     }
-    $this->render('f_permintaan',array('model'=>$model));
-}
+
 }
