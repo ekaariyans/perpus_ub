43775dc6700597264063222b636668abe14aa4cc
experizhal <experizhal@gmail.com>
2014-08-22 21:02:15 +0700

    view

--- protected/views/permintaan/f_permintaan.php
+++ protected/views/permintaan/f_permintaan.php
@@ -15,8 +15,15 @@
 	'enableAjaxValidation'=>false,
 )); ?>
 
-	<?php echo $form->errorSummary($model); ?>
-        
+	
+<ul class="nav nav-tabs" role="tablist">
+  <li class="active"><a href="#Buku" role="tab" data-toggle="tab">MAHASISWA</a></li>
+  <li><a href="#Fakultas" role="tab" data-toggle="tab">FAKULTAS</a></li>
+</ul>
+
+<!-- Tab panes -->
+<div class="tab-content">
+  <div class="tab-pane active" id="Buku"><?php echo $form->errorSummary($model); ?>
     <div class="box box-primary">
         <div class="box-header">
             <h4 class="box-title">PERMINTAAN BUKU BARU</h4>
@@ -107,6 +114,37 @@
             </div>
         </div>
     </div>
+    </div>
+    
+  <div class="tab-pane" id="Fakultas">
+  <div class="box box-primary">
+        <div class="box-header">
+            <h4 class="box-title">PERMINTAAN BUKU BARU</h4>
+        </div>
+  <?php echo $form->errorSummary($model); ?>
+	
+	<div class="form-group">
+		<?php echo $form->labelEx($model,'id_anggota',array('class' => 'col-sm-2 control-label')); ?>
+		<div class="col-sm-10">
+		<?php echo $form->textField($model,'id_anggota',array('class' => 'form-control','placeholder' => 'id_anggota')); ?>
+		<?php echo $form->error($model,'id_anggota'); ?>
+		</div>
+	</div>
+    
+    <div class="form-group">
+        <div class="col-sm-10">
+        <b>Masukkan Data Excel :</b>
+        <?php echo $form->fileField($model,'filee',array('size'=>60,'maxlength'=>200,'class' => 'col-sm-offset-2')); ?>
+       </div>
+    </div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Submit'); ?>
+	</div>
+  </div>
+  </div>
+</div>
+	
 
 <?php $this->endWidget(); ?>
 
